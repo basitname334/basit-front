c3be7bcababe3d3a79e17f12c6a7b24b214d00d3
Abdul Basit Dogar <220578100+basitname334@users.noreply.github.com>
2025-11-01 00:50:40 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
 })
 
